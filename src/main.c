51fdbc0ff0ab13083ded8e71651ae3e2a3f3f4aa
Mitsutaka Shiraishi <srismttk@gmail.com>
2018-08-25 17:14:39 +0900

    Get Input Matrix row/line size, elements from input_matrix.txt

--- src/main.c
+++ src/main.c
@@ -3,39 +3,71 @@
 #include <string.h>
 
 #define INPUT_MATRIX "input_matrix.txt"
+#define BUF_LEN 1024
 
-int split_string_to_int(char *target, char *split, int *ret) {
-	char *p;
-	int i = 1;
-
-	p = strtok(target, split);
-	ret[0] = atoi(p);
-
-	while( p != NULL) {
-		p = strtok(NULL, split);
-
-		if(p != NULL) {
-			ret[i] = atoi(p);
-			i++;
-		}
-	}
-
-	return 0;
-}
+extern int *init_matrix(int row, int line);
+extern void free_square_matrix(int *matrix);
+extern int get_matrix_elements(int *matrix, char *input, int now_line, int max_row);
 
 int main(void) {
-	char input[1000];
-
+	char input[BUF_LEN];
+	int *input_matrix;
+	int now_input_line = 0;
+	int now_matrix_line = 0;
+	int row, line;
 	FILE *fp;
+
 	if ( (fp = fopen(INPUT_MATRIX, "r") ) == NULL ) {
 		printf("Error: Open %s\n", INPUT_MATRIX);
 		return -1;
 	}
 	
 	while( fgets(input, sizeof(input), fp) != NULL) {
-		printf("%s\n", input);
+
+		if(now_input_line == 0) {
+			line = atoi(input);
+
+		} else if(now_input_line == 1) {
+			row = atoi(input);
+
+			// initialize square matrix
+			printf("matrix size, line %d row %d\n", line, row);
+			input_matrix = init_matrix(row, line);
+
+		} else {
+
+			if(now_matrix_line < line) {
+				printf("Line %d : %s", now_matrix_line, input);
+
+				if( !(get_matrix_elements(input_matrix, input, now_matrix_line, row)) ) {
+					printf("Invalid Matrix\n");
+					return -1;
+				}
+				
+			} else {
+				printf("Max Matrix Line is %d, so line %d is ignored\n",
+				       line, now_matrix_line);
+			}
+			
+			now_matrix_line++;
+
+		}
+
+		now_input_line++;
 	}
+
+	printf("Input Matrix Element : %d %d\n", row, line);
+	for(int i = 0; i < row; i++) {
+		for(int j = 0; j < line; j++) {
+			printf("%d ", input_matrix[i * row + j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+
 	fclose(fp);
+	free_square_matrix(input_matrix);
 
 	return 0;
 }
---
+++ src/matrix.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *init_matrix(int row, int line) {
+	int *matrix;
+	matrix = malloc ( sizeof(int *) * line * row);
+	memset (matrix, 0, sizeof(int *) * line * row);
+
+	return matrix;
+}
+
+void free_square_matrix(int **matrix) {
+	free(matrix);
+}
+
+int split_string_to_int(char *target, char *split, int *ret, int ret_size) {
+	char *p;
+	int i = 1;
+
+	p = strtok(target, split);
+	ret[0] = atoi(p);
+
+	while( p != NULL) {
+		p = strtok(NULL, split);
+
+		if(p != NULL) {
+
+			if(i > ret_size) {
+				return 0;
+			}
+
+			ret[i] = atoi(p);
+			i++;
+		}
+	}
+
+	return 1;
+}
+
+int get_matrix_elements(int *matrix, char *input, int now_line, int max_row) {
+	int *int_array;
+
+	int_array = malloc(sizeof(int) * max_row);
+	memset(int_array, 0, sizeof(int) * max_row);
+
+	if ( !(split_string_to_int (input, " ", int_array, max_row)) ) {
+		printf("Max Matrix Length is %d, this Matrix[%s] Length is over\n", max_row, input);
+		return 0;
+	}
+
+	for(int i = 0; i < max_row; i++) {
+		matrix[now_line * max_row + i] = int_array[i];
+	}
+
+	free(int_array);
+	
+	return 1;
+}
