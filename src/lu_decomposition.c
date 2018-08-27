cc400a6e18db34113ff197a1cbd8becfd4fdd581
Mitsutaka Shiraishi <srismttk@gmail.com>
2018-08-27 21:48:22 +0900

    算出したLUが、掛け合わせることで正しくAになるか確認するcheck_lu_matrix_correctを追加

--- src/lu_decomposition.c
+++ src/lu_decomposition.c
@@ -5,6 +5,32 @@
 extern int *init_square_matrix(int length);
 extern void free_square_matrix(int *matrix);
 
+int check_lu_matrix_correct (int *matrix_a, int *matrix_l, int *matrix_u, int matrix_length) {
+
+  for(int i = 0; i < matrix_length; i++) {
+    for(int j = 0; j < matrix_length; j++) {
+      int lu_total = 0;
+      int end;
+
+      if(i > j)
+	end = i;
+      else
+	end = j;
+
+      for(int k = 0; k <= end; k++)
+	lu_total += matrix_l[i * matrix_length + k] * matrix_u[k * matrix_length + j];
+
+      if(lu_total != matrix_a[i * matrix_length + j]) {
+	printf("Invalid LU Matrix, A(%d %d)=%d, LU=%d\n", i ,j, matrix_a[i * matrix_length + j], lu_total);
+	return 0;
+      }
+
+    }
+  }
+  
+  return 1;
+}
+
 int get_lu_decomposition_matrix(int *matrix_a, int *matrix_l, int *matrix_u, int matrix_length) {
 
   for(int i = 0; i < matrix_length; i++) {
@@ -13,11 +39,10 @@ int get_lu_decomposition_matrix(int *matrix_a, int *matrix_l, int *matrix_u, int
       if(i > j) {
 	int l_matrix_multi = 0;
 
-	if(j > 0) {
-	  // Low matrix Multi = L(i,1) * U(1,j) + ... + L(i, j-1) * U(j-1,j)
-	  for(int k = 0; k < j; k++)
-	    l_matrix_multi += matrix_l[i * matrix_length + k] * matrix_u[k * matrix_length + j];
-	}
+	// Low matrix Multi = L(i,1) * U(1,j) + ... + L(i, j-1) * U(j-1,j)
+	for(int k = 0; k < j; k++)
+	  l_matrix_multi += matrix_l[i * matrix_length + k] * matrix_u[k * matrix_length + j];
+
 	// L (i,j) = ( A(i,j) - low_matrix_multi ) / U(j,j)
 	matrix_l[i * matrix_length + j] = (matrix_a[i * matrix_length + j] - l_matrix_multi) / matrix_u[j * matrix_length + j];
 
@@ -28,11 +53,10 @@ int get_lu_decomposition_matrix(int *matrix_a, int *matrix_l, int *matrix_u, int
 	  matrix_l[i * matrix_length + j] = 1;
 	}
 
-	if(i > 0) {
-	  // Upper matrix Multi = L(i,1) * U(1,j) + ... L(i, i-1) * U(i-1,j)
-	  for(int k = 0; k < i; k++)
-	    u_matrix_multi += matrix_l[i * matrix_length + k] * matrix_u[k * matrix_length + j];
-	}
+	// Upper matrix Multi = L(i,1) * U(1,j) + ... L(i, i-1) * U(i-1,j)
+	for(int k = 0; k < i; k++)
+	  u_matrix_multi += matrix_l[i * matrix_length + k] * matrix_u[k * matrix_length + j];
+
 	// U (i,j) = A(i,j) - upper_matrix_multi
 	matrix_u[i * matrix_length + j] = matrix_a[i * matrix_length + j] - u_matrix_multi;
 
@@ -60,6 +84,8 @@ int get_lu_decomposition_matrix(int *matrix_a, int *matrix_l, int *matrix_u, int
     printf("\n");
   }
   printf("\n");
+
+  check_lu_matrix_correct(matrix_a, matrix_l, matrix_u, matrix_length);
 #endif
 
   return 1;
